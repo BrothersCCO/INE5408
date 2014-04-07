e1e22241a145d3b02a95dba72f250e463678cb34
Ranieri Althoff <ranisalt@gmail.com>
2014-04-06 22:38:19 -0300

    Proposed grade: 90 / 90

--- Trabalho 2/main.cpp
+++ Trabalho 2/main.cpp
@@ -1,8 +1,10 @@
 #include "Interface.hpp"
 #include "Queue.hpp"
 
+#define MAXFILA 10
+
 int main() {
-	Queue<DataItem> q(100);
+	Queue<DataItem> q(MAXFILA);
 	Interface i(&q);
 	i.run();
 	return 0;
